ff9d8dea15d1a3c456fa6a4ce474f2e4f3fb9791
gauarv <gkg603348gmail.com>
2026-08-31 15:06:04 +0530

    Remove hardcoded credentials and use env variables

--- database.py
+++ database.py
@@ -3,9 +3,9 @@ import os
 import pymysql
 
 conn = pymysql.connect(
-    host=os.getenv("DB_HOST", "127.0.0.1"),
-    user=os.getenv("DB_USER", "root"),
-    password=os.getenv("DB_PASSWORD", "root"),
+    host=os.getenv("DB_HOST", "mysql-19172a6e-gkg603348-5e56.c.aivencloud.com"),
+    user=os.getenv("DB_USER", "avnadmin"),
+    password=os.getenv("DB_PASSWORD", ""),
     database=os.getenv("DB_NAME", "school"),
-    port=int(os.getenv("DB_PORT", 3306))
+    port=int(os.getenv("DB_PORT", 28680))
 )
