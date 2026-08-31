51f978a32945d31e039cd336b274e90a26b6b1ae
gauarv <gkg603348gmail.com>
2026-08-31 12:12:06 +0530

    update project

--- database.py
+++ database.py
@@ -1,10 +1,11 @@
+
+import os
 import pymysql
 
-
 conn = pymysql.connect(
-    host="127.0.0.1",
-    user="root",
-    password="root",            
-    database="school",    
-    port=3306  
+    host=os.getenv("DB_HOST", "127.0.0.1"),
+    user=os.getenv("DB_USER", "root"),
+    password=os.getenv("DB_PASSWORD", "root"),
+    database=os.getenv("DB_NAME", "school"),
+    port=int(os.getenv("DB_PORT", 3306))
 )
